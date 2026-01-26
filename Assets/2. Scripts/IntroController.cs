a854b117ddd8c07b4e21e46324dbcf174b42801f
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2026-01-26 17:41:15 +0900

    커밋
    영상 추가

--- Assets/2. Scripts/IntroController.cs
+++ Assets/2. Scripts/IntroController.cs
@@ -12,16 +12,15 @@ public class IntroController : MonoBehaviour
         // 영상이 끝났을 때 실행될 이벤트 연결
         videoPlayer.loopPointReached += OnVideoFinished;
     }
-    private void Start()
+    void Start()
     {
         skipBtn.onClick.AddListener(() => OnVideoFinished(videoPlayer));
-
     }
 
     void OnVideoFinished(VideoPlayer vp)
     {
         // 다음 씬으로 넘어가거나 UI를 전환합니다.
-       Debug.Log("인트로 끝! 메인 화면으로 이동합니다.");
+       //Debug.Log("인트로 끝! 메인 화면으로 이동합니다.");
        GameSceneManager.Instance.LoadScene("StartScene");
     }
 

36c280e380269c7671ed85aa6e622b387d5066e0
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2026-01-26 12:27:04 +0900

    커밋
    인트로씬

---
+++ Assets/2. Scripts/IntroController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class IntroController : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public Button skipBtn;
+
+    void OnEnable()
+    {
+        // 영상이 끝났을 때 실행될 이벤트 연결
+        videoPlayer.loopPointReached += OnVideoFinished;
+    }
+    private void Start()
+    {
+        skipBtn.onClick.AddListener(() => OnVideoFinished(videoPlayer));
+
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        // 다음 씬으로 넘어가거나 UI를 전환합니다.
+       Debug.Log("인트로 끝! 메인 화면으로 이동합니다.");
+       SceneManager.LoadScene("StartScene");
+    }
+
+    void OnDisable()
+    {
+        videoPlayer.loopPointReached -= OnVideoFinished;
+    }
+}
---
+++ Assets/2. Scripts/Manager/UIToolkitManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIToolkitManager : MonoBehaviour
+{
+    UIDocument document;
+    VisualElement panel;
+
+    Button startBtn;
+
+    [Header("Fade In 시간")]
+    float fadeDuration = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        document = GetComponent<UIDocument>();
+        panel = document.rootVisualElement;
+
+        startBtn = panel.Q<Button>("PlayBtn");
+
+        startBtn.clickable.clicked += GameStart;
+
+        //스타트 씬 Fadein 효과
+        StartCoroutine(FadeInUi());
+    }
+
+    IEnumerator FadeInUi()
+    {
+        //시작 시 UI의 투명도를 0으로 설정
+        panel.style.opacity = 0;
+
+        yield return null;
+
+        float elapsed = 0f;
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            panel.style.opacity = Mathf.Clamp01(elapsed / fadeDuration);
+            yield return null;
+        }
+        panel.style.opacity = 1;
+    }
+
+    void GameStart()
+    {
+        print("게임시작");
+    }
+}
